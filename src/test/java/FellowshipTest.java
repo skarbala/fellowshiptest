3bd6173d5495a09657a346a4ae5701affbc7b78a
martin.skarbala <skarbala.martin@gmail.com>
2018-12-07 13:58:16 +0100

    added test for checking age and hobbit names

--- src/test/java/FellowshipTest.java
+++ src/test/java/FellowshipTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FellowshipTest {
-  WebDriver driver;
+  private WebDriver driver;
 
   @Before
   public void setUp() {
@@ -21,11 +21,20 @@ public class FellowshipTest {
   }
 
   @Test
+  public void shouldContainInitialPointsValue() {
+    String expectedInitialValue = "25";
+    String actualInitialValue = driver.findElement(By.cssSelector("div.points-left h2")).getText();
+
+    Assert.assertEquals(expectedInitialValue, actualInitialValue);
+  }
+
+  @Test
   public void shouldShowAllFellowshipMembers() {
     List<String> fellowMembers = driver.findElements(By.cssSelector("ul.list-of-fellows div.col h1"))
         .stream()
         .map(WebElement::getText)
         .collect(Collectors.toList());
+
     Assert.assertEquals(9, fellowMembers.size());
     Assert.assertTrue(fellowMembers.contains("Frodo"));
     Assert.assertTrue(fellowMembers.contains("Samwise"));
@@ -36,5 +45,35 @@ public class FellowshipTest {
     Assert.assertTrue(fellowMembers.contains("Boromir"));
     Assert.assertTrue(fellowMembers.contains("Meriadoc"));
     Assert.assertTrue(fellowMembers.contains("Peregrin"));
+  }
+
+  @Test
+  public void shouldContainAgeForEachFellow() {
+    List<String> fellowMembersAgeList = driver.findElements(By.cssSelector("ul.list-of-fellows h2:nth-child(3)"))
+        .stream()
+        .map(WebElement::getText)
+        .map(fellowAge -> fellowAge.replace("Age:", "").trim())
+        .collect(Collectors.toList());
+
+    Assert.assertFalse(fellowMembersAgeList.isEmpty());
+    fellowMembersAgeList.forEach(fellowMembersAge -> {
+      Assert.assertFalse(fellowMembersAge.equals(""));
+      Assert.assertFalse(fellowMembersAge.isEmpty());
+    });
+  }
+
+  @Test
+  public void shouldContainOnlySpecificHobbits() {
+    String[] hobbits = {"Frodo", "Samwise", "Meriadoc", "Peregrin"};
+
+    List<String> actualHobbits = driver.findElements(By.cssSelector("ul.list-of-fellows li"))
+        .stream()
+        .filter(webElement -> webElement.findElement(By.cssSelector("h2:nth-child(2)")).getText().contains("Hobbit"))
+        .map(webElement -> webElement.findElement(By.cssSelector("h1")).getText())
+        .collect(Collectors.toList());
+
+    for (String hobbit : hobbits) {
+      Assert.assertTrue(actualHobbits.contains(hobbit));
+    }
   }
 }
